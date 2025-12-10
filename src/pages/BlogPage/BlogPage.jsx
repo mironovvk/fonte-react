bc1ff7230911cb27849a935623306020895ee093
Artem Sokolov <the.jelll.2006@gmail.com>
2025-12-10 19:48:52 +0500

    refactor: remove PostList from page

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -1,11 +1,9 @@
 import "./BlogPage.scss";
-import PostList from "@/modules/PostList";
-import { postsMock } from "@/mocks/posts.mock.js";
 
 const BlogPage = () => {
   return (
     <>
-      <PostList cards={postsMock} />
+
     </>
   );
 };
