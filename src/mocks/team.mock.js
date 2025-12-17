ae8e60b46c5da45ae6184ca9fa18ce0d461833cd
Kirill Mironov <mironovk255@gmail.com>
2025-12-18 00:32:30 +0500

    feat: add mock for Contacts block

---
+++ src/mocks/team.mock.js
@@ -0,0 +1,41 @@
+import erjanImg from '@/assets/images/team/erjan.png';
+import oljasImg from '@/assets/images/team/oljas.png';
+
+export const teamMembersMock = [
+  {
+    id: 1,
+    image: erjanImg,
+    name: 'Ержан Мусин',
+    position: 'CEO',
+  },
+  {
+    id: 2,
+    image: oljasImg,
+    name: 'Олжас Укенов',
+    position: 'CFO',
+  },
+  {
+    id: 3,
+    image: erjanImg,
+    name: 'Ержан Мусин',
+    position: 'CEO',
+  },
+  {
+    id: 4,
+    image: oljasImg,
+    name: 'Олжас Укенов',
+    position: 'CFO',
+  },
+  {
+    id: 5,
+    image: erjanImg,
+    name: 'Ержан Мусин',
+    position: 'CEO',
+  },
+  {
+    id: 6,
+    image: oljasImg,
+    name: 'Олжас Укенов',
+    position: 'CFO',
+  },
+];
