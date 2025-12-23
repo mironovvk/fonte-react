652a9f3c2c2b4eede0778320b6a5d2e108515de3
Kirill Mironov <mironovk255@gmail.com>
2025-12-23 23:30:55 +0500

    feat: add About Us block to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import './HomePage.scss';
 import Hero from '@/modules/Hero';
 import BlogSection from '@/modules/BlogSection';
+import AboutSection from '@/modules/AboutSection';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <AboutSection />
       <BlogSection />
     </>
   );
