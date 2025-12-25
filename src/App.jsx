2b54763833f861d7a32993be0435d758a2c22987
Kirill Mironov <mironovk255@gmail.com>
2025-12-25 09:10:02 +0500

    feat: add snowfall effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { navLinks } from '@/mocks/nav.mock.js';
 import Content from '@/components/Content';
+import Snowfall from 'react-snowfall';
 
 function App() {
   return (
     <>
+      <Snowfall color='#82c3d9' />
       <Header navLinks={navLinks} />
       <Content />
       <Footer navLinks={navLinks} />
