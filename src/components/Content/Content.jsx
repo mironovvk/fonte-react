9606fde6fb57e218fe1fae4fa876953e52d93673
Kirill Mironov <mironovk255@gmail.com>
2025-12-25 09:20:35 +0500

    feat: add Our Team page

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,6 +1,7 @@
 import './Content.scss';
 import { Routes, Route } from 'react-router-dom';
 import BlogPage from '@/pages/BlogPage';
+import TeamPage from '@/pages/TeamPage';
 import ProductPage from '@/pages/ProductPage';
 import HomePage from '@/pages/HomePage';
 import ContactPage from '@/pages/ContactPage';
@@ -11,6 +12,7 @@ const Content = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<div>О нас</div>} />
+        <Route path="/team" element={<TeamPage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/contacts" element={<ContactPage />} />
